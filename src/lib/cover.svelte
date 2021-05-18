90ddf9dfc8b673a6903917c563f6025121cafeb7
Torsten Dittmann <torsten.dittmann@googlemail.com>
2021-05-18 09:30:27 +0200

    fix cover

--- src/lib/cover.svelte
+++ src/lib/cover.svelte
@@ -6,10 +6,11 @@
 	div.cover {
 		display: flex;
 		grid-area: cover;
-		background-image: url(https://picsum.photos/1080);
+		background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.6)), url(https://picsum.photos/1080);
 		background-size: cover;
-		background-position: center;
+		background-position: center center;
 		align-items: center;
 		justify-content: center;
+		color: white;
 	}
 </style>
